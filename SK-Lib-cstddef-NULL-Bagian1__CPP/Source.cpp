94abe5560d3f34eafbc4d5294b4dc8dc3988058d
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-01-11 08:26:11 +0700

    Add project files.

---
+++ SK-Lib-cstddef-NULL-Bagian1__CPP/Source.cpp
@@ -0,0 +1,15 @@
+#include <conio.h>
+
+using namespace std;
+
+/*
+    Source by GeeksForGeeks
+    Modified For Learn by RK
+    I.D.E : VS2019
+*/
+
+int main() {
+	int* num = 0;
+    _getch();
+	return 0;
+}
